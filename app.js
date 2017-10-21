1d42f99d869be395cf8782869f8bc542bebe6a7e
Jaybee6 <jayabaskar.rajagopal@gmail.com>
2017-10-21 13:54:42 -0400

    Js format and addig package-lock.json

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -8,24 +5,24 @@ var io = require('socket.io')(server);
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', function(client){    
-    
-    client.on('join', function(name){        
+io.on('connection', function(client) {
+
+    client.on('join', function(name) {
         client.name = name;
         client.emit("message", name + " has joined the chat.");
-         client.broadcast.emit("message", name + " has joined the chat.");
+        client.broadcast.emit("message", name + " has joined the chat.");
     });
 
-    client.on('messages', function(data){
-            client.broadcast.emit("messages", {name :client.name, message: data});            
-            client.emit('mymessages', {name :client.name, message: data});
+    client.on('messages', function(data) {
+        client.broadcast.emit("messages", { name: client.name, message: data });
+        client.emit('mymessages', { name: client.name, message: data });
     });
-    
+
 });
 
-app.get('/', function(request, response){
+app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
-    
+
 });
 
 
@@ -42,9 +39,8 @@ server.listen(8080);
 //         }, 2000);
 //     }, 5000);
 
-    
+
 //     console.log("new request came in : " + request);
 // });
 // server.listen(8080);
 // console.log("server is running on 8080");
-
